a16514dd4148f75b9fbb7b6ecceb95c76ea4b2fd
jackson-stamper <jacksonstamper32@gmail.com>
2021-09-16 10:21:31 -0500

    Switch branch finally up to date!

--- A2JacksonStamper.appstudio/forms/Form1/Form1.js
+++ A2JacksonStamper.appstudio/forms/Form1/Form1.js
@@ -5,7 +5,9 @@ These messages are stored in an array. Each message is an item in the array.
 You will create two versions of the program - one using if/else statement(s) and one using switch statement(s).
 Put each program on a different form in the same project. 
 */
+
 //declaring variables
+
 let fName = prompt("What is your first name")
 let state = prompt("Enter your state's 2 letter abbreviation")
 let temp = Number(prompt("What is the current temperature at your location?"))
@@ -15,11 +17,38 @@ let messages = ["wear a warm coat, hat, scarf and gloves.",
                 "wear a warm coat but you won't need a hat, a scarf, and warm gloves", 
                 "wear your warmest coat, a warm hat, a scarf, and warm gloves", 
                 "wear a warm coat, hat and gloves. Maybe a scarf too."]
-
+/*
 //if else statements
-if (state = "NE" && temp < 32) 
-  console.log(`${fName}, ${messages[0]}.`)
-if else (state = "NE" && temp > 32 && 
+  if(state == "NE" && temp < 32) 
+    console.log(`${fName},${messages[0]}`)
+  else if (state == "NE" && (temp > 32 && temp < 50)) 
+    console.log(`${fName},${messages[1]}`)
+  else if (state == "FL" && (temp > 32 && temp < 50)) 
+    console.log(`${fName},${messages[2]}`)
+  else if (state == "FL" && (temp > 50 && temp < 70)) 
+    console.log(`${fName},${messages[3]}`)
+  else
+    console.log(`sorry ${fName}, there is no data available.`)
+*/
+
+//Swith version of the above code
+switch (true) {
+  case state == 'NE' && temp < 32:
+    console.log(`${fName},${messages[0]}`)
+  break
+  case state == 'NE' && temp >= 32 && temp <= 50:
+    console.log(`${fName},${messages[1]}`)
+  break
+  case state == 'FL' && temp >= 32 && temp <= 50: 
+    console.log(`${fName},${messages[2]}`)
+  break
+  case state = 'FL' && temp > 50 && temp <= 70:
+    console.log(`${fName},${messages[3]}`)
+  break
+  default:
+    console.log(`sorry ${fName}, there is no data available.`)
+  break
+}
 
 
 
